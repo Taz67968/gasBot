5e0c0f12ae39afbe17490f7fd373ccf86b9a8a78
Taz67968 <tazohcliff@gmail.com>
2026-06-01 16:47:49 +0100

    fixing issues with redis

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -4,9 +4,10 @@ import { OrderService } from './order.service';
 import { Order } from '@/database/entities/order.entity';
 import { Payment } from '@/database/entities/payment.entity';
 import { Customer } from '@/database/entities/customer.entity';
+import { CustomerModule } from '@/customer/customer.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Payment, Customer])],
+  imports: [TypeOrmModule.forFeature([Order, Payment, Customer]), CustomerModule],
   providers: [OrderService],
   exports: [OrderService],
 })
